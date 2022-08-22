83acb20a8f138de175bbc37a2cdf722f988dff97
Mr.M.V.BALA <mvbalakrishnan95@gmail.com>
2022-08-22 18:30:19 +0530

    it is my udemy exercise

--- src/DecimalComparator.java
+++ src/DecimalComparator.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class DecimalComparator {
+public class DecimalComparator {
+    public static boolean areEqualByThreeDecimalPlaces(double firstNumber, double secondNumber) {
+        int a = (int)(firstNumber * 1000);
+        int b = (int)(secondNumber * 1000);
+        if (a == b)
+            return true;
+        else
+            return false;
+
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        DecimalComparator.areEqualByThreeDecimalPlaces(1.213,1.2134);
     }
 }
